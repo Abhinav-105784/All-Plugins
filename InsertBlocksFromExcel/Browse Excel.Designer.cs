6221341f2a1d916b067996f9cdfad4dad39e1e01
Abhinav-105784 <167738096+Abhinav-105784@users.noreply.github.com>
2024-05-06 15:28:35 +0530

    Add files via upload

---
+++ InsertBlocksFromExcel/Browse Excel.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace InsertBlocksFromExcel8
+{
+    partial class Browse_Excel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.File = new System.Windows.Forms.Label();
+            this.Sheet = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(795, 325);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 354);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(620, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // File
+            // 
+            this.File.AutoSize = true;
+            this.File.Location = new System.Drawing.Point(12, 357);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(35, 16);
+            this.File.TabIndex = 7;
+            this.File.Text = "File :";
+            // 
+            // Sheet
+            // 
+            this.Sheet.AutoSize = true;
+            this.Sheet.Location = new System.Drawing.Point(12, 399);
+            this.Sheet.Name = "Sheet";
+            this.Sheet.Size = new System.Drawing.Size(48, 16);
+            this.Sheet.TabIndex = 8;
+            this.Sheet.Text = "Sheet :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(68, 396);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 24);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(68, 453);
+            this.browse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(121, 36);
+            this.browse.TabIndex = 10;
+            this.browse.Text = "Browse...";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(717, 354);
+            this.Open.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 34);
+            this.Open.TabIndex = 11;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(720, 399);
+            this.Close.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(72, 33);
+            this.Close.TabIndex = 12;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Browse_Excel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 500);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.browse);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Sheet);
+            this.Controls.Add(this.File);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Browse_Excel";
+            this.Text = "Browse_Excel";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label File;
+        private System.Windows.Forms.Label Sheet;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Close;
+    }
+}
---
+++ InsertBlocksFromExcel/InsertBlocks.cs
@@ -0,0 +1,101 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+using ExcelDataReader;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+using Autodesk.Civil;
+
+namespace InsertBlocksFromExcel8
+{
+    public class InsertBlocks
+    {
+        [CommandMethod("InsertBlocksFromExcel")]
+        public void ShowForm()
+        {
+            Browse_Excel browse = new Browse_Excel();
+            browse.Show();
+        }
+
+        public static void Blocks(string selectedFile)
+        {
+            Document document = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument;
+            Database database = document.Database;
+            Editor editor = document.Editor;
+
+            using (Transaction transaction = database.TransactionManager.StartTransaction())
+            {
+                using (var stream = File.Open(selectedFile, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        DataSet dataset = reader.AsDataSet(new ExcelDataSetConfiguration()
+                        {
+                            ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                        });
+                        var dataTable = dataset.Tables[0];
+                        string columnHeader1 = dataTable.Columns[0].ColumnName;
+                        string columnHeader2 = dataTable.Columns[1].ColumnName;
+                        string columnHeader3 = dataTable.Columns[2].ColumnName;
+                        string columnHeader4 = dataTable.Columns[3].ColumnName;
+                        string columnHeader5 = dataTable.Columns[4].ColumnName;
+                       
+                        if (columnHeader1 == "Block Name" && columnHeader2 == "x" && columnHeader3 == "y" && columnHeader4 == "z" && columnHeader5 == "Rotation")
+                        {
+                            document.LockDocument();
+                            BlockTable bt = transaction.GetObject(database.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                            for (int row = 0; row < dataTable.Rows.Count; row++)
+                            {
+                                string blockName = dataTable.Rows[row][0].ToString();
+                                double x = Convert.ToDouble(dataTable.Rows[row][1]);
+                                double y = Convert.ToDouble(dataTable.Rows[row][2]);
+                                double z = Convert.ToDouble(dataTable.Rows[row][3]);
+                                double rotation = Convert.ToDouble(dataTable.Rows[row][4]);
+                                double rotationRads = rotation * 3.14159265 / 180;
+                                
+                                if (bt.Has(blockName))
+                                {
+
+                                    BlockTableRecord btr = transaction.GetObject(database.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+                                    BlockReference br = new BlockReference(new Point3d(x, y, z), bt[blockName])
+                                    {
+                                        ScaleFactors = new Scale3d(1.0, 1.0, 1.0),
+                                        Rotation = rotationRads,
+                                        
+                                    };
+                                    
+                                    transaction.GetObject(database.BlockTableId, OpenMode.ForWrite);
+                                    btr.AppendEntity(br);
+                                    transaction.AddNewlyCreatedDBObject(br, true);
+                                    document.Editor.UpdateScreen();
+                                    editor.WriteMessage($"\nBlock {blockName} inserted");
+
+                                }
+                                else
+                                {
+                                    editor.WriteMessage($"\nBlock '{blockName}' not found.");
+                                    return;
+                                }
+
+
+                            }
+                        }
+
+
+                        else
+                        {
+                            MessageBox.Show("The Excel file is not in the proper format. Please make sure the column headers are correct.", "Excel Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                    }
+                }
+                transaction.Commit();
+            }
+        }
+    }
+}
