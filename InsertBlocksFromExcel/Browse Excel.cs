3fe14c39a681f9d9aff02d2e800737c2c6874e98
Abhinav-105784 <167738096+Abhinav-105784@users.noreply.github.com>
2024-08-22 10:40:18 +0530

    Add files via upload

---
+++ InsertBlocksFromExcel/Browse Excel.cs
@@ -0,0 +1,86 @@
+﻿using ExcelDataReader;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+namespace InsertBlocksFromExcel8
+{
+    public partial class Browse_Excel : Form
+    {
+        public Browse_Excel()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            string selectedFile = textBox1.Text;
+
+            if(!string.IsNullOrEmpty(selectedFile))
+            {
+                InsertBlocks.Blocks(selectedFile);
+                MessageBox.Show("Blocks Inserted succesfully");
+            }
+            else
+            {
+                MessageBox.Show("Please select the excel file");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Data.DataTable dt = dataTable[comboBox1.SelectedItem.ToString()];
+            dataGridView1.DataSource = dt;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        DataTableCollection dataTable;
+        private void browse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                openFile.Filter = "Excel Files| *.xls;*.xlsm;*.xlsx";
+                openFile.Title = "Select an excel File";
+
+                if(openFile.ShowDialog()==DialogResult.OK)
+                {
+                    textBox1.Text = openFile.FileName;
+                    using (var stream = System.IO.File.Open(openFile.FileName, System.IO.FileMode.Open,FileAccess.Read))
+                    {
+                        using(IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+
+                            });
+                          
+                            dataTable = result.Tables;
+                            comboBox1.Items.Clear();
+                            foreach (DataTable table in dataTable)
+                            {
+                                comboBox1.Items.Add(table.TableName);
+                            }
+
+                        }
+                          
+                    }
+                }
+            }
+
+        }
+    }
+}
--- InsertBlocksFromExcel/InsertBlocks.cs
+++ InsertBlocksFromExcel/InsertBlocks.cs
@@ -43,12 +43,13 @@ namespace InsertBlocksFromExcel8
                         string columnHeader3 = dataTable.Columns[2].ColumnName;
                         string columnHeader4 = dataTable.Columns[3].ColumnName;
                         string columnHeader5 = dataTable.Columns[4].ColumnName;
-                       
-                        if (columnHeader1 == "Block Name" && columnHeader2 == "x" && columnHeader3 == "y" && columnHeader4 == "z" && columnHeader5 == "Rotation")
+                        //string columnHeader6 = dataTable.Columns[5].ColumnName;
+
+                        if (columnHeader1 == "Block Name" && columnHeader2 == "x" && columnHeader3 == "y" && columnHeader4 == "z" && columnHeader5 == "Rotation" )//&& columnHeader6=="Layer")
                         {
                             document.LockDocument();
                             BlockTable bt = transaction.GetObject(database.BlockTableId, OpenMode.ForRead) as BlockTable;
-
+                            LayerTable lt = transaction.GetObject(database.LayerTableId, OpenMode.ForRead) as LayerTable;
                             for (int row = 0; row < dataTable.Rows.Count; row++)
                             {
                                 string blockName = dataTable.Rows[row][0].ToString();
@@ -56,42 +57,47 @@ namespace InsertBlocksFromExcel8
                                 double y = Convert.ToDouble(dataTable.Rows[row][2]);
                                 double z = Convert.ToDouble(dataTable.Rows[row][3]);
                                 double rotation = Convert.ToDouble(dataTable.Rows[row][4]);
+                                //string layer = dataTable.Rows[row][5].ToString();
                                 double rotationRads = rotation * 3.14159265 / 180;
-                                
                                 if (bt.Has(blockName))
                                 {
-
+                                    if (!lt.Has(blockName))
+                                    {
+                                        LayerTableRecord layerRecord = new LayerTableRecord
+                                        {
+                                            Name = blockName
+                                        };
+                                        lt.UpgradeOpen();
+                                        lt.Add(layerRecord);
+                                        transaction.AddNewlyCreatedDBObject(layerRecord, true);
+                                    }
                                     BlockTableRecord btr = transaction.GetObject(database.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
                                     BlockReference br = new BlockReference(new Point3d(x, y, z), bt[blockName])
                                     {
                                         ScaleFactors = new Scale3d(1.0, 1.0, 1.0),
                                         Rotation = rotationRads,
+                                        Layer = blockName,
                                         
+
                                     };
-                                    
                                     transaction.GetObject(database.BlockTableId, OpenMode.ForWrite);
                                     btr.AppendEntity(br);
                                     transaction.AddNewlyCreatedDBObject(br, true);
                                     document.Editor.UpdateScreen();
                                     editor.WriteMessage($"\nBlock {blockName} inserted");
-
                                 }
                                 else
                                 {
                                     editor.WriteMessage($"\nBlock '{blockName}' not found.");
                                     return;
                                 }
-
-
+                                
                             }
                         }
-
-
                         else
                         {
                             MessageBox.Show("The Excel file is not in the proper format. Please make sure the column headers are correct.", "Excel Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
-
                     }
                 }
                 transaction.Commit();
